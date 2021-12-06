3c88c25921fc076b6fd15b23f0192a47b857889b
Sergei S <div@altertech.com>
2021-12-06 01:45:10 +0100

    aes 256

--- examples/udp_aes.rs
+++ examples/udp_aes.rs
@@ -1,5 +1,5 @@
 use aes_gcm::aead::{Aead, NewAead};
-use aes_gcm::{Aes128Gcm, Key, Nonce};
+use aes_gcm::{Aes256Gcm, Key, Nonce};
 use std::net::UdpSocket;
 
 fn main() {
@@ -7,13 +7,13 @@ fn main() {
     let aes_key =
         hex::decode("26fd38045707792a9bc50f3761a58987c4a9362cf60389f341c28e37b1125d93").unwrap();
     let aes_nonce = hex::decode("5c088bee9dea47a5ee31c2eb").unwrap();
-    let key = Key::from_slice(&aes_key[0..16]);
-    let cipher = Aes128Gcm::new(key);
+    let key = Key::from_slice(&aes_key);
+    let cipher = Aes256Gcm::new(key);
     let nonce = Nonce::from_slice(&aes_nonce);
     let mut packet: Vec<u8> = Vec::new();
     packet.extend_from_slice(&psrt::CONTROL_HEADER);
     packet.extend_from_slice(&psrt::PROTOCOL_VERSION.to_le_bytes());
-    packet.push(psrt::AUTH_KEY_AES128_GCM);
+    packet.push(psrt::AUTH_KEY_AES256_GCM);
     packet.extend_from_slice("user1".as_bytes());
     packet.push(0x0);
     let mut data: Vec<u8> = vec![psrt::OP_PUBLISH_NO_ACK, psrt::DEFAULT_PRIORITY];
